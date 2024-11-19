1aa8c4b4c5376b12212e90d4c421516ee0f07ef5
Bisdcode <bismarck.sdcode@gmail.com>
2024-11-19 13:21:35 -0300

    Initial commit

---
+++ http_react/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+
+import './App.css'
+
+function App() {
+  // 1 - resgatando dados
+  const [products, setProducts] = useState([]);
+
+  
+
+  return (
+    <>
+      <div className='App'>
+        <h1>HTTP em React</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
