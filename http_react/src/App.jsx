cb5b5ca859404039688d5f0e65cb70848bcd2eb5
Bisdcode <bismarck.sdcode@gmail.com>
2024-11-26 21:24:56 -0300

    add hook useFetch

--- http_react/src/App.jsx
+++ http_react/src/App.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react'
 
+import { useFetch } from './hooks/useFetch';
+
 const url = "http://localhost:3000/products"
 
 import './App.css'
@@ -8,39 +10,63 @@ function App() {
   // salvar e controlar os dados
   const [products, setProducts] = useState([]);
 
-  // resgatar os dados da Api
-  useEffect(() => {
+  // 1 - resgatar os dados da Api
+  
+  // 4 - custom hook
+  // renomeando o data para items
+  const {data: items} = useFetch(url)
+
+  // Substituido pelo custom hook
+  // useEffect(() => {
     
-    // função assincrona em "fetch", recurso do javascript para chamadas assincronas
-    async function getData() {
+  //   // função assincrona em "fetch", recurso do javascript para chamadas assincronas
+  //   async function getData() {
       
-      const res = await fetch(url);
+  //     const res = await fetch(url);
 
-      const data = await res.json();
+  //     const data = await res.json();
 
-      // console.log(data);
-      setProducts(data);
-    }
+  //     // console.log(data);
+  //     setProducts(data);
+  //   }
 
-    // executando a função de resgatar os dados
-    getData();
-  }, []);
+  //   // executando a função de resgatar os dados
+  //   getData();
+  // }, []);
 
-  // envio de dados
+  // 2 - envio de dados
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
 
-  const handleSubmit = (e) => {
+  // função para o envio dos dados do formulário
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
+    // objeto com os dados dos 'states'
     const product = {
       name,
       price,
     };
 
-    console.log(product);
-  };
+    // console.log(product);
 
+    // variavel POST
+    const res = await fetch(url, {
+      // configuração da requisição
+      method: "POST",
+      // padronizando os dados trafegados entre aplicação e api
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // dados da requisição. enviando em formato de 'texto'
+      body: JSON.stringify(product),
+    });
+  
+    // 3 - carregamento dinâmico
+    const addedProduct = await res.json();
+
+    setProducts ((prevProducts) => [...prevProducts, addedProduct]);
+  };
 
   return (
     <>
@@ -48,7 +74,8 @@ function App() {
         <h1>HTTP em React</h1>
         {/* apresentação dos dados */}
         <ul>
-          {products.map ((product) => (
+          
+          {items?.map ((product) => (
             <li key={product.id}>
               {product.name} - R${product.price}
             </li>
